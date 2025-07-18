573c7c0eee4d77e3d4a6f84698904b7814e2da8b
FernandoPen <f281180@dac.unicamp.br>
2025-07-17 23:47:54 -0300

    adicionado postgreSQL e github

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ import javascriptLogo from './assets/javascript.svg';
 import openglLogo from './assets/opengl.svg';
 import cLogo from './assets/c.svg';
 import c__Logo from './assets/c--.svg';
+import postgresLogo from './assets/postgresql.svg';
+import githubLogo from './assets/github.svg';
 
 
 function App() {
@@ -23,12 +25,14 @@ function App() {
               <Habilidade nome="FastAPI" logo={fastapiLogo} link="https://fastapi.tiangolo.com/" />
               <Habilidade nome="Figma" logo={figmaLogo} link="https://www.figma.com/pt-br/" />
               <Habilidade nome="Python" logo={pythonLogo} link="https://www.python.org/" />
+              <Habilidade nome='PostgreSQL' logo={postgresLogo} link="https://www.postgresql.org/" />
           </div>
           <div className="container">
               <Habilidade nome="JavaScript" logo={javascriptLogo} link="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript" />
               <Habilidade nome="OpenGL" logo={openglLogo}  link="https://www.opengl.org/" />
               <Habilidade nome="C++" logo={cLogo} link="https://learn.microsoft.com/pt-br/cpp/cpp/?view=msvc-170" />
               <Habilidade nome="C#" logo={c__Logo} link="https://learn.microsoft.com/pt-br/dotnet/csharp/" />
+              <Habilidade nome="GitHub" logo={githubLogo} link="https://github.com/" />
           </div>
       </>
   );
